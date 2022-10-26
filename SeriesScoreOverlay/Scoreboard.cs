9fdb6079b02d9756870ef78d1d97f1d84331ee1d
Kevin Mattix <kmattrider@gmail.com>
2022-10-26 18:13:25 -0400

    created model

---
+++ SeriesScoreOverlay/Scoreboard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeriesScoreOverlay
+{
+    public class Scoreboard
+    {
+        public string homeName, awayName;
+        public int homeScore, awayScore;
+        public bool visable;
+        
+        public Scoreboard(string _homeName, string _awayName)
+        {
+            homeName = _homeName;
+            awayName = _awayName;
+            homeScore = 0;
+            awayScore = 0;
+            visable = false;
+        }
+    }
+}
