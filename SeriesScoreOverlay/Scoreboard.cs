99ce48721b76cf0feccb248b754c2cf24030a9e6
kmattix <kmattrider@gmail.com>
2022-11-09 15:58:54 -0500

    removed unused imports

--- SeriesScoreOverlay/Scoreboard.cs
+++ SeriesScoreOverlay/Scoreboard.cs
@@ -1,12 +1,4 @@
-﻿using Microsoft.VisualBasic;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace SeriesScoreOverlay
 {
